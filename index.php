641a09fb393ced6e6ed45d9e9a65bacea5fb76ff
bryan kennedy <bkennedy@smm.org>
2014-09-08 22:12:14 -0500

    Adding expanatory notes to HTML

--- index.php
+++ index.php
@@ -10,49 +10,29 @@ define("LDAP_YEAR", 1601);
 // Unix time starts in 1970
 define("UNIX_YEAR", 1970);
 
-$years_between_unix_ldap = UNIX_YEAR - LDAP_YEAR;
-$outputs['Years between Unix and LDAP start date'] = $years_between_unix_ldap;
-
-$days_between_unix_ldap = $years_between_unix_ldap * DAYS_IN_YEAR;
-$outputs['Days between Unix and LDAP start date'] = $days_between_unix_ldap;
-
-$seconds_between_unix_ldap = $days_between_unix_ldap * SEC_IN_DAY;
-$outputs['Seconds between Unix and LDAP start date'] = $seconds_between_unix_ldap;
-
-$nanoseconds_between_unix_ldap = $seconds_between_unix_ldap * NANOSEC_IN_SEC;
-$outputs['Nanoseconds between Unix and LDAP start date'] = $nanoseconds_between_unix_ldap;
-
-$ldap_units_between_unix_ldap = $nanoseconds_between_unix_ldap / 100;
-$outputs['LDAP units between Unix and LDAP start date'] = $ldap_units_between_unix_ldap;
-
-$outputs['1'] = '------------------------';
-
-$seconds_since_unix = time();
-$outputs['Seconds from unix start date til now'] = $seconds_since_unix;
-
-$nanoseconds_since_unix = $seconds_since_unix * NANOSEC_IN_SEC;
-$outputs['Nanoseconds from Unix start date til now'] = $nanoseconds_since_unix;
-
-$nanoseconds_since_ad = $nanoseconds_since_unix + $nanoseconds_between_unix_ldap;
-$outputs['Nanoseconds since LDAP'] = $nanoseconds_since_ad;
+$years_btwn_unix_ldap = UNIX_YEAR - LDAP_YEAR;
+$days_btwn_unix_ldap = $years_btwn_unix_ldap * DAYS_IN_YEAR;
+$seconds_btwn_unix_ldap = $days_btwn_unix_ldap * SEC_IN_DAY;
+$nanosec_btwn_unix_ldap = $seconds_btwn_unix_ldap * NANOSEC_IN_SEC;
 
 /**
  * Working backwards from the Apache Directory Studio information
  */
-$outputs['2'] = '------------------------';
-$from_apache_dir_studio = 130277434357040000;
-$outputs['From Apache Directory Studio - LDAP units'] = $from_apache_dir_studio;
-$from_apache_nanosec = $from_apache_dir_studio * 100;
-$outputs['From Apache - nanosecs'] = $from_apache_nanosec;
-$from_apache_unix_sec = ($from_apache_nanosec - $nanoseconds_between_unix_ldap) / NANOSEC_IN_SEC;
-$outputs['From Apache - Unix secs'] = $from_apache_unix_sec;
+$test_ldap_time = 130277434357040000;
+$outputs['Test LDAP time'] = $test_ldap_time;
+$test_ldap_time_nanosecs = $test_ldap_time * 100;
+$outputs['Test LDAP time - nanosecs'] = $test_ldap_time_nanosecs;
+$test_ldap_time_unix = ($test_ldap_time_nanosecs - $nanosec_btwn_unix_ldap) / NANOSEC_IN_SEC;
+$outputs['Test LDAP time - Unix'] = $test_ldap_time_unix;
 
 date_default_timezone_set('America/Chicago');
-$from_apache_english = date('l jS \of F Y h:i:s A', $from_apache_unix_sec);
-$outputs['From Apache - English'] = $from_apache_english;
+$from_apache_english = date('l jS \of F Y h:i:s A', $test_ldap_time_unix);
+$outputs['Test LDAP time - English'] = $from_apache_english;
 
 // Now in LDAP time
-$ldap_now = ($nanoseconds_between_unix_ldap + (time() * NANOSEC_IN_SEC))/ 100;
+// LDAP time is broken down into 100 nanosecond chunks.
+// I'm just doing the math this way to make that clear.
+$ldap_now = ($nanosec_btwn_unix_ldap + (time() * NANOSEC_IN_SEC))/ 100;
 $outputs['LDAP Now'] = $ldap_now;
 $ldap_in_180_days = $ldap_now + (((SEC_IN_DAY * 180) * NANOSEC_IN_SEC) / 100);
 $outputs['LDAP in 180 days'] = $ldap_in_180_days;
@@ -68,7 +48,7 @@ $expire_unix = time() + (SEC_IN_DAY * 180);
 echo '<table>';
 foreach ($outputs as $name => $value) {
   echo '<tr>';
-  echo '<td align="right"><strong>' . $name . '</strong></td>';
+  echo '<td align="left"><strong>' . $name . '</strong></td>';
   echo '<td>';
   if (is_numeric($value)) {
     echo number_format($value, 0, '', '');
@@ -83,7 +63,7 @@ echo '</table>';
 //$seconds_from_apache_dir_studio = $from_apache_dir_studio / NANOSEC_IN_SEC;
 //$outputs['Seconds from Apache Dir Studio'] = $seconds_from_apache_dir_studio;
 
-//$unix_seconds = $seconds_from_apache_dir_studio - $seconds_between_unix_ldap;
+//$unix_seconds = $seconds_from_apache_dir_studio - $seconds_btwn_unix_ldap;
 //$outputs['Unix seconds'] = $unix_seconds;
 
 //$outputs['From the web'] = '130277027370000000';
